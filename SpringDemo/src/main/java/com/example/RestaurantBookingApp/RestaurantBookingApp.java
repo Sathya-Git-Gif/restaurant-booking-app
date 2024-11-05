45832803ac6d2d2596e0371ba64e9bd43105c12d
Sathya-Git-Gif <duckyducksathya@gmail.com>
2024-11-05 17:03:47 -0600

    Starting the base application

---
+++ SpringDemo/src/main/java/com/example/RestaurantBookingApp/RestaurantBookingApp.java
@@ -0,0 +1,14 @@
+package com.example.RestaurantBookingApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBookingApp.class, args);
+        BookingController controller = new BookingController();
+        controller.startServer();
+    }
+}
